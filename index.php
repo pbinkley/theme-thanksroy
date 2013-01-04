5b16110d670acaa5e9d476f43ddded68fced86f1
Peter Binkley <peter.binkley@ualberta.ca>
2013-01-03 23:09:57 -0700

    initial customization

--- index.php
+++ index.php
@@ -1,39 +1,60 @@
-<?php head(array('bodyid'=>'home', 'bodyclass' =>'two-col')); ?>
+<?php head(array('bodyid'=>'home', 'bodyclass' =>'two-col-primary')); ?>
 <div id="primary">
     <?php if ($homepageText = get_theme_option('Homepage Text')): ?>
     <p><?php echo $homepageText; ?></p>
     <?php endif; ?>
-    <?php if (get_theme_option('Display Featured Item') == 1): ?>
-    <!-- Featured Item -->
-    <div id="featured-item">
-        <?php echo display_random_featured_item(); ?>
-    </div><!--end featured-item-->	
-    <?php endif; ?>
 
-    <!-- Recent Items -->		
-    <div id="recent-items">
-        <h2><?php echo __('Recently Added Items'); ?></h2>
-        <?php 
-        $homepageRecentItems = (int)get_theme_option('Homepage Recent Items') ? get_theme_option('Homepage Recent Items') : '3';
-        set_items_for_loop(recent_items($homepageRecentItems));
-        if (has_items_for_loop()): 
-        ?>
-        <ul class="items-list">
-        <?php while (loop_items()): ?>
-        <li class="item">
-            <h3><?php echo link_to_item(); ?></h3>
-            <?php if($itemDescription = item('Dublin Core', 'Description', array('snippet'=>150))): ?>
-                <p class="item-description"><?php echo $itemDescription; ?></p>
-            <?php endif; ?>						
-        </li>
-        <?php endwhile; ?>
-        </ul>
-        <?php else: ?>
-        <p><?php echo __('No recent items available.'); ?></p>
+<!-- list collections - based on http://omeka.org/codex/Functions/loop_collections -->
+   <h2>Collections</h2>
+
+    <div class="pagination"><?php echo pagination_links(); ?></div>
+    <?php set_collections_for_loop(recent_collections(5)); ?>
+    <?php while (loop_collections()): ?>
+    <div class="collection">
+
+        <h2><?php echo link_to_collection(); ?></h2>
+
+        <div class="element">
+           <!-- <h3><?php echo __('Description'); ?></h3> -->
+            <div class="element-text"><?php echo nls2p(collection('Description', array('snippet'=>150))); ?></div>
+        </div>
+
+        <?php if(collection_has_collectors()): ?>
+        <div class="element">
+            <h3><?php echo __('Collector(s)'); ?></h3>
+            <div class="element-text">
+                <p><?php echo collection('Collectors', array('delimiter'=>', ')); ?></p>
+            </div>
+        </div>
         <?php endif; ?>
-        <p class="view-items-link"><?php echo link_to_browse_items(__('View All Items')); ?></p>
-    </div><!-- end recent-items -->
+        
+        <div class="collection-items">
+<?php
+$items = get_items(array('collection'=>get_current_collection()->id, 'featured' => true), 1);
+set_items_for_loop($items);
+while(loop_items()):
+if (item_has_thumbnail()): 
+    echo link_to_collection_for_item(item_square_thumbnail(),  array('class' => 'collection-img')); 
+endif; 
+endwhile; 
+?>	
+</div>	
+
+
+        <p class="view-items-link"><?php echo link_to_browse_items(__('View the items in %s', collection('Name')), array('collection' => collection('id'))); ?></p>
+
+        <?php echo plugin_append_to_collections_browse_each(); ?>
+
+    </div><!-- end class="collection" -->
+    <?php endwhile; ?>
+
+    <?php echo plugin_append_to_collections_browse(); ?>
+
+
+
+    <!-- Recent Items - suppressed pbinkley -->		
 </div><!-- end primary -->
+
 <div id="secondary">
     <?php if (get_theme_option('Display Featured Collection')): ?>
     <!-- Featured Collection -->
@@ -46,5 +67,20 @@
     <?php echo exhibit_builder_display_random_featured_exhibit(); ?>
     <?php endif; ?>
     </div><!-- end recent-collections -->
+    <?php if (get_theme_option('Display Featured Item') == 1): ?>
+    <!-- Featured Item -->
+    <div id="featured-item">
+    <!-- note: we've modifed the function display_random_featured_items in application/helpers/ItemFunctions.php
+     to include a link to the collection:
+    
+     set_current_item($randomItem);
+     $html .= '<h4 style="clear: both;">&gt;&gt; ' . link_to_collection_for_item() . '</h4>';
+
+      (inserted right before the return)    
+      (and commented out the description)
+    -->
+    <?php echo display_random_featured_item(); ?>
+    </div><!--end featured-item-->	
+    <?php endif; ?>
 </div><!-- end secondary -->
 <?php foot(); ?>
