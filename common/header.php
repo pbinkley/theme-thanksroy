cc3e6b3699f99fbcccb989da1d8218397d5a2e50
Peter Binkley <peter.binkley@ualberta.ca>
2013-01-07 23:17:48 -0700

    added popups to markers

--- common/header.php
+++ common/header.php
@@ -34,13 +34,25 @@
 
           var geocoder = new google.maps.Geocoder();
           var locations = address.split("|");
+          var markers = [];
+          var infowindow = new google.maps.InfoWindow();
+          
           for (var i = 0; i < locations.length; i++) {
             geocoder.geocode( { 'address': locations[i]}, function(results, status) {
                if (status == google.maps.GeocoderStatus.OK) {
                   var marker = new google.maps.Marker({
                      map: map,
-                     position: results[0].geometry.location
+                     position: results[0].geometry.location,
+                    title: results[0].formatted_address
                   });
+                  
+                  google.maps.event.addListener(marker, 'click', function() {
+                     infowindow.setContent("<span class='map_popup'>" + results[0].formatted_address + "</span>");
+                     infowindow.open(map, marker);
+                  });
+
+                  markers.push(marker);
+
                   bounds.extend(results[0].geometry.location);
                } else {
 //            alert('Geocode was not successful for the following reason: ' + status);
